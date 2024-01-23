c6ebc51c81c9799c0f1ed89fd67153ed52cfdee3
Angela Ferrer <gelferrero01@gmail.com>
2024-01-24 01:18:20 +0800

    Check whether sum or product will be returned, show result

--- exercise_one.py
+++ exercise_one.py
@@ -3,18 +3,28 @@
 
 # Calculate the prduct and sum of two given numbers
 
-def calculate (first_number, second_number)
+def calculate(first_number,second_number):
     # Calculate product
     product = first_number * second_number
     # Calculate sum
     sum = first_number + second_number
+    # Check if product is equal to or lower than 1000. 
+    if product <= 1000:
+        return product
+    else:
+        return sum
 
 # First pair of numbers
 
 first_number = 20
 second_number = 30
+result = calculate (first_number, second_number)
+print ("The result is", result)
 
 # Second pair of numbers
 
 first_number = 40
 second_number = 30
+result = calculate 
+print ("The result is", result)
+
