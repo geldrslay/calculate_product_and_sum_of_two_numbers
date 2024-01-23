12b66d33bd01e8954b8c6fd40be3dcda2394f4ef
Angela Ferrer <gelferrero01@gmail.com>
2024-01-24 01:08:55 +0800

    calculate and given

---
+++ exercise_one.py
@@ -0,0 +1,20 @@
+# Calculate the multiplication and sum of two numbers
+# Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
+
+# Calculate the prduct and sum of two given numbers
+
+def calculate (first_number, second_number)
+    # Calculate product
+    product = first_number * second_number
+    # Calculate sum
+    sum = first_number + second_number
+
+# First pair of numbers
+
+first_number = 20
+second_number = 30
+
+# Second pair of numbers
+
+first_number = 40
+second_number = 30
