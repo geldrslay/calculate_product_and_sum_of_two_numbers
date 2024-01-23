5157d9098fee6f9dc7fdbe2e497e721567f86cdf
Angela Ferrer <gelferrero01@gmail.com>
2024-01-24 01:22:51 +0800

    final

--- exercise_one.py
+++ exercise_one.py
@@ -1,8 +1,6 @@
 # Calculate the multiplication and sum of two numbers
 # Given two integer numbers, return their product only if the product is equal to or lower than 1000. Otherwise, return their sum.
 
-# Calculate the prduct and sum of two given numbers
-
 def calculate(first_number,second_number):
     # Calculate product
     product = first_number * second_number
@@ -25,6 +23,6 @@ print ("The result is", result)
 
 first_number = 40
 second_number = 30
-result = calculate 
+result = calculate (first_number,second_number)
 print ("The result is", result)
 
